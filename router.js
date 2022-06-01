86f97306951b8d996efeb520177104d3fd9dd178
Пользователь <simpla@ukr.net>
2022-06-01 17:40:01 +0300

    server

---
+++ router.js
@@ -0,0 +1,21 @@
+import Router from 'express';
+import PostController from "./PostControllers.js";
+
+const router = new Router()
+
+// router.get('/', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.send('Hello World');
+//   })
+  
+//   router.listen("3000", function () {
+//     console.log('alive');
+//   })
+
+router.put('/users', PostController.update)
+router.get('/users/:id', PostController.getOne)
+router.get('/', PostController.getAll)
+
+
+export default router;
+
